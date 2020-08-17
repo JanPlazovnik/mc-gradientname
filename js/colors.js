c869578d018ab2ba4dc56869718672728f55732a
Jan Plazovnik <jan.plazovnik@gmail.com>
2020-08-17 20:31:32 +0200

    add fields to input hex colors, update js for feedback

--- js/colors.js
+++ js/colors.js
@@ -2,7 +2,7 @@ function getColoredName(str, col1, col2) {
   let out = '';
   let html = '';
   for (let i = 0; i < str.length; i++) {
-    let t = i / str.length; 
+    let t = i / str.length;
     let col = [
       Math.floor(col1[0] + (col2[0] - col1[0]) * t),
       Math.floor(col1[1] + (col2[1] - col1[1]) * t),
@@ -10,7 +10,7 @@ function getColoredName(str, col1, col2) {
     ];
     // console.log(col)
     out += '&#' + col.map(x => (x < 16 ? "0" : "") + x.toString(16)).join('') + "&l" + str[i];
-    html += `<b><span style="color:#${col.map(x => (x < 16 ? "0" : "") + x.toString(16)).join('')}">${ str[i] }</span></b>`.split('/').join('');
+    html += `<b><span style="color:#${col.map(x => (x < 16 ? "0" : "") + x.toString(16)).join('')}">${str[i]}</span></b>`.split('/').join('');
   }
   return [out, html]
 }
